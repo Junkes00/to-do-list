2f7da361768309777f406f381b83b97663e604d4
Nicolas Junkes <nicolas.junkes00@gmail.com>
2022-12-28 23:46:38 -0300

    Create component to add new tasks

---
+++ src/Components/AddTask/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Styles.css";
+
+const AddTask = () => {
+  return (
+    <div className="add-task-container">
+      <input className="add-task-input" type="text" name="" id="" />
+      <button className="add-task-button">Add new Task</button>
+    </div>
+  );
+};
+
+export default AddTask;
