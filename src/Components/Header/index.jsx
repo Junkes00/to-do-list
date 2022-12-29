51d819e43acbacdbb4b55978a78ecd0358db0f7f
Nicolas Junkes <nicolas.junkes00@gmail.com>
2022-12-28 23:45:24 -0300

    Create Header component

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="header-container">My tasks</h1>
+    </div>
+  );
+};
+
+export default Header;
