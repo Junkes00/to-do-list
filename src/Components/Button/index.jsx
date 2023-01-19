ce7fd2b6784a0a818ec74a868c53a9de13cbf2a3
Nicolas Junkes <nicolas.junkes00@gmail.com>
2023-01-18 23:41:13 -0300

    Create button to add new tasks

---
+++ src/Components/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles.css";
+
+const Button = () => {
+  return (
+    <>
+      <button className="button">New task</button>
+    </>
+  );
+};
+
+export default Button;
