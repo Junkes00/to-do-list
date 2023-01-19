e6927281e3df8efd0e52cb532216d24fd072dc20
Nicolas Junkes <nicolas.junkes00@gmail.com>
2023-01-18 23:44:24 -0300

    Create handleAddNewTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import AddTask from "./Components/AddTask";
 import Header from "./Components/Header/index";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddNewTask = (taskTitle) => {
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: 1,
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  };
+
   return (
     <div className="app-container">
       <Header />
-      <AddTask />
+      <AddTask handleAddNewTask={handleAddNewTask} />
     </div>
   );
 }
